47fbdaaffd5ff3da743634010a018053fea967ce
ali qaderi <qaderiir@gmail.com>
2024-01-12 12:54:40 +0330

    remove old codes

--- wp-content/themes/kitkala/404.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-
-<div class="content404">
-  <h1 class="title404">عه مثل اینکه این صفحه وجود نداره</h1>
-  <img src="<?php bloginfo('template_url'); ?>/assets/img/404.png" alt="">
-  <p>اشکال نداره حالا که این صفحه وجود نداره می تونی از بخش پایین وارد صفحه اصلی بشی</p>
-  <a class="gotohome" href="<?php bloginfo('url'); ?>">بریم صفحه اصلی</a>
-</div>
-<?php get_footer(); ?>
--- wp-content/themes/kitkala/sidebar.php
+++
@@ -1,9 +0,0 @@
- <!-- سایدبار صفحه -->
- <aside class="kitkala-sidebar">
-
-     <?php if(is_active_sidebar('sidebar_widget')){
-
-            dynamic_sidebar('sidebar_widget');
-        } ?>
-
- </aside>
