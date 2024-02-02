b25221b13936ab5b5d3557a01158ae0db894d28e
ali qaderi <qaderiir@gmail.com>
2024-02-02 16:40:08 +0330

    پایان 26

---
+++ wp-content/themes/kitkala/page.php
@@ -0,0 +1,13 @@
+
+<?php get_header( ) ?>
+<?php get_sidebar( ) ?>
+      <section class="pages-content">
+        <h1 class="page-title"><?php the_title() ?></h1>
+        <p class="content-pa">
+      <?php the_content() ?>
+      </section>
+
+    </div>
+
+    <div class="clear"></div>
+    <?php get_footer( ) ?>
---
+++ wp-content/themes/kitkala/sidebar.php
@@ -0,0 +1,5 @@
+<aside class="kitkala-sidebar">
+            <?php  if(is_active_sidebar('sidebar_widget')){
+                dynamic_sidebar('sidebar_widget');
+            }?>
+      </aside>
